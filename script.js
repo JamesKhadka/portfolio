b0feda79334ccbee13af247fc83474cdbefaf068
JamesKhadka <khadkajames0@gmail.com>
2024-03-06 05:21:23 +0545

    OK

--- script.js
+++ script.js
@@ -40,21 +40,21 @@ setInterval(changeText, 3000);
 // circle skill ////////////////////////////////////////////////////////
 
 const circles = document.querySelectorAll('.circle');
-circles.forEach(elem=>{
+circles.forEach(elem => {
     var dots = elem.getAttribute("data-dots");
     var marked = elem.getAttribute("data-percent");
-    var percent = Math.floor(dots*marked/100);
+    var percent = Math.floor(dots * marked / 100);
     var points = "";
     var rotate = 360 / dots;
 
 
-    for(let i = 0 ; i < dots ; i++){
-    points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+    for (let i = 0; i < dots; i++) {
+        points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
     }
     elem.innerHTML = points;
 
     const pointsMarked = elem.querySelectorAll('.points');
-    for(let i = 0; i<percent ; i++){
+    for (let i = 0; i < percent; i++) {
         pointsMarked[i].classList.add('marked')
     }
 })
@@ -71,55 +71,55 @@ let menuLi = document.querySelectorAll('header ul li a');
 let section = document.querySelectorAll('section');
 
 
-function activeMenu(){
+function activeMenu() {
     let len = section.length;
-    while(--len && window.scrollY + 97 < section[len].offsetTop){};
+    while (--len && window.scrollY + 97 < section[len].offsetTop) { };
     menuLi.forEach(sec => sec.classList.remove("active"));
     menuLi[len].classList.add("active");
 }
 
 activeMenu();
-window.addEventListener("scroll",activeMenu);
+window.addEventListener("scroll", activeMenu);
 
 
 // sticky navbar /////////////////////////////////////////////////
 const header = document.querySelector("header");
-window.addEventListener("scroll",function(){
-    header.classList.toggle("sticky",window.scrollY > 50)
+window.addEventListener("scroll", function () {
+    header.classList.toggle("sticky", window.scrollY > 50)
 })
 
 // toggle icon navbar ////////////////////////////////////////////////
 let menuIcon = document.querySelector("#menu-icon");
 let navlist = document.querySelector(".navlist");
 
-menuIcon.onclick = ()=>{
+menuIcon.onclick = () => {
     menuIcon.classList.toggle("bx-x");
     navlist.classList.toggle("open");
 }
 
 
-window.onscroll = ()=>{
+window.onscroll = () => {
     menuIcon.classList.remove("bx-x");
     navlist.classList.remove("open");
 }
 
 // parallax ////////////////////////////////////////////////
 
-const observer = new IntersectionObserver((entries)=>{
-    entries.forEach((entry)=>{
-        if(entry.isIntersecting){
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
             entry.target.classList.add("show-items");
-        }else{
+        } else {
             entry.target.classList.remove("show-items");
         }
     });
 });
 
 const scrollScale = document.querySelectorAll(".scroll-scale");
-scrollScale.forEach((el)=>observer.observe(el));
+scrollScale.forEach((el) => observer.observe(el));
 
 const scrollBottom = document.querySelectorAll(".scroll-bottom");
-scrollBottom.forEach((el)=>observer.observe(el));
+scrollBottom.forEach((el) => observer.observe(el));
 
 const scrollTop = document.querySelectorAll(".scroll-top");
-scrollTop.forEach((el)=>observer.observe(el));
+scrollTop.forEach((el) => observer.observe(el));
